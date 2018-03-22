289de993eddf931d7540036d847ffb65842311c6
Jaydip <jaydip.jadhav@nihilent.com>
2018-03-22 15:41:02 +0530

    Initial Checkin

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService, AuthenticationService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'login.component.html'
+})
+
+export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    returnUrl: string;
+    loginAsList: any;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService) { }
+
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+        this.loginAsList = [
+            {
+                "Id": 1,
+                "Name": "Team"
+            },
+            {
+                "Id": 2,
+                "Name": "Panel"
+            },
+            {
+                "Id": 3,
+                "Name": "Associate"
+            }]
+            this.model.loginAsId=1
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
+    }
+}
