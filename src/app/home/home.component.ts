3a70df1b999d05e06350d5f2efbda1f02ae31658
Jaydip <jaydip.jadhav@nihilent.com>
2018-07-10 12:55:38 +0530

    Fix npm issues

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { User } from '../_models/index';
 import { UserService } from '../_services/index';
+import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -27,19 +28,24 @@ export class HomeComponent implements OnInit {
     }
 
     receiveMessage($event) {
+        console.log('In receiveMessage Method');
         this.ActiveTabs = $event
     }
 
     changeMessageEvent(mssgEvent) {
-        this.ActiveTab=mssgEvent.ActiveTabChildParam;
+        console.log('In receiveMessage Method');
+        this.ActiveTab = mssgEvent.ActiveTabChildParam;
     }
-    constructor(private userService: UserService) {
-        console.log(localStorage.getItem('currentUser'));
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    constructor(private userService: UserService, private router: Router) {
+        let cachedUser = localStorage.getItem('currentUser');
+        if (!cachedUser)
+            this.router.navigate(['/login'])
+        else
+            this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
-
+        console.log('In ngOnInit Method');
         this.userService.GetAssociateNewRequest(this.currentUser.id).subscribe(result => {
             this.NewRequestCount = result;
             this.NewRequestCount = Array.from(this.NewRequestCount).length;
